1bc6943ef556c664d9d48b91db78a52115dad680
Fernanman <97917922+Fernanman@users.noreply.github.com>
2022-01-18 11:08:22 -0500

    Update ServerV2.py

--- ServerV2.py
+++ ServerV2.py
@@ -12,15 +12,19 @@ def ConnectPlayer(socket, conn, players):
     players += 1
 
     with conn:
+        print('Current players:', players)
         print('Connected by', addr)
 
         while True:
             data = conn.recv(1024)
             conn.sendall(data)
-
+            
+print("Ready to accept Connections.")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+
+    
 
     start_new_thread(ConnectPlayer(s, conn, players))
